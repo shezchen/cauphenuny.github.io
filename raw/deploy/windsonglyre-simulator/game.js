b53540ac51afa8a7b087e03602919bb8e72da21b
Cauphenuny <starseekist@gmail.com>
2024-06-18 19:49:32 +0800

    Site updated: 2024-06-18 19:49:31

--- raw/deploy/windsonglyre-simulator/game.js
+++ raw/deploy/windsonglyre-simulator/game.js
@@ -1,5 +1,8 @@
+let tape = localStorage.getItem('tape');
 document.getElementById("difficulty").innerHTML = "难度：" + localStorage.getItem('difficulty');
-document.getElementById("sheet").innerHTML = "谱子：" + localStorage.getItem('sheet');
+document.getElementById("sheet").innerHTML = "谱子：" + tape;
+tape = JSON.parse(tape);
+let env = JSON.parse(localStorage.getItem('env'));
 
 import { keyup_animation, keydown_animation } from './keyboard.js'
 
@@ -14,8 +17,12 @@ for (var i = 1; i <= 7; i++) {
     buttom.style.transform = "translate(-50%, -50%)";
 }
 
+import { context, drum } from "./player.js";
+
+init_constants();
+
 document.addEventListener("keydown", function(event) {
-    var key = event.key;
+    var key = event.key.toUpperCase();
     var code = key.charCodeAt();
     console.log(`${key} ${code} down`);
     if (event.repeat) {
@@ -24,43 +31,18 @@ document.addEventListener("keydown", function(event) {
     if (event.ctrlKey || event.altKey || event.metaKey) {
         return;
     }
-    if (code >= 97 && code <= 122) {
-        code -= 32; // 小写转大写
-    }
     if (key2note.has(code)) {
-        dm.start({ note: "hihat-close" });
+        drum.start({ note: "hihat-close" });
         keydown_animation(code);
     }
 });
 
 document.addEventListener("keyup", function(event) {
-    var key = event.key;
+    var key = event.key.toUpperCase();
     var code = key.charCodeAt();
     console.log(`${key} ${code} up`);
-    if (code >= 97 && code <= 122) {
-        code -= 32; // 小写转大写
-    }
     if (key2note.has(code)) {
         keyup_animation(code);
-    }
-});
-
-import { DrumMachine } from "https://unpkg.com/smplr/dist/index.mjs";
-
-const context = new AudioContext();
-const dm = new DrumMachine(context);
-
-dm.load.then(() => {
-    console.log("loaded.");
-    //dm.start({ note: "hihat-close" });
-    //const now = context.currentTime;
-    //dm.getVariations("hihat-open").forEach((variation, index) => {
-    //  dm.start({ note: variation, time: now + index + 1 });
-    //});
-    init_constants();
-    const hovers = document.getElementsByClassName("hvinfo");
-    for (var i = 0; i < hovers.length; i++) {
-        hovers[i].style.display = "none";
     }
 });
 
---
+++ raw/deploy/windsonglyre-simulator/index.js
@@ -0,0 +1,235 @@
+import {
+    key, note_name, sharp_name, flat_name,
+    sharp_note, sharp_scale_name, flat_note, flat_scale_name,
+    diff, velocity_levels, velocity_adj, key2note, C1, C2, C3,
+    init_constants,
+} from './constants.js'
+import { env, play, notepress, notedown, noteup, piano } from './player.js'
+import { keyup_animation, keydown_animation, mouseenter, mouseleave } from './keyboard.js'
+
+
+function refresh() {
+    document.getElementById("bpm").value = env.bpm;
+    document.getElementById("vel").textContent = "力度：" + velocity_levels[env.velocity];
+    const selectElement = document.getElementById('offset_option');
+    const selectedOption = selectElement.options[selectElement.selectedIndex];
+    if (selectedOption.value == "flex") {
+        document.getElementById("key_name").innerHTML = "(1=" + note_name[(env.global_offset + 120) % 12] + ")";
+    } else { 
+        var cnt = parseInt(document.getElementById("key_offset").value);
+        if (cnt >= 0) {
+            if (cnt > 6) cnt = 6;
+            document.getElementById("key_name").innerHTML = sharp_scale_name[cnt] + "大调 <img alt=\"调号\" class=\"keysgn-img\"" + 
+                                                                                              "align=\"center\" " + 
+                                                                                              "src=\"./keysignature/" + cnt + ".png\"" + 
+                                                                                         ">";
+        } else if (cnt < 0) {
+            if (cnt < -6) cnt = -6;
+            document.getElementById("key_name").innerHTML = flat_scale_name[-cnt] + "大调 <img alt=\"调号\" class=\"keysgn-img\"" + 
+                                                                                              "align=\"center\" " + 
+                                                                                              "src=\"./keysignature/" + cnt + ".png\"" + 
+                                                                                         ">";
+        } else {
+        }
+    }
+    document.getElementById("time_sign1").value = env.time1;
+    document.getElementById("time_sign2").value = env.time2;
+}
+function init() {
+    env.velocity = 4;
+    env.global_offset = 0;
+    env.bpm = 90;
+    env.time1 = 4, env.time2 = 4;
+    document.getElementById('offset_option').selectedIndex = 0;
+    env.fixed_offset.fill(0);
+    document.getElementById("key_offset").value = "0";
+    document.getElementById("input").value = "";
+    document.getElementById("input2").value = "";
+    refresh();
+}
+function extract(tape) {
+    console.log("------- start extracting -------");
+    console.log(`origin: \n ${tape} \n`);
+    var ext = "";
+    for (var i = 0; i < tape.length; i++) {
+        switch (tape[i]) {
+            case '#':
+                while (i < tape.length && tape[i] != '\n' && tape[i] != '\r') {
+                    i++;
+                }
+                break;
+            case '(': case ')':
+            case '[': case ']':
+            case '{': case '}':
+            case '<': case '>':
+            case '-': case '+':
+            case '^': case '%':
+            case '.': case '/':
+                ext += tape[i];
+                break;
+            default: 
+                if (key2note.has(tape.charCodeAt(i))) {
+                    ext += tape[i];
+                }
+                break;
+        }
+    }
+    return ext;
+}
+function decompress(sheet) {
+    //TODO
+    return sheet;
+}
+function after_load() {
+    document.getElementById("status").style = "color: green;";
+    document.getElementById("status").innerHTML = "准备就绪";
+    const hovers = document.getElementsByClassName("hvinfo");
+    for (var i = 0; i < hovers.length; i++) {
+        hovers[i].style.display = "none";
+    }
+    init();
+    refresh();
+
+    const key_buttons = document.getElementsByClassName("kb-img");
+    for (var i = 0; i < key_buttons.length; i++) {
+        key_buttons[i].addEventListener('mousedown', function() {
+            var key_code = this.id.charCodeAt("key".length);
+            notepress(key_code, key2note.get(key_code), env.velocity);
+        });
+        key_buttons[i].addEventListener('mouseenter', function() {
+            mouseenter(this.parentNode);
+        });
+        key_buttons[i].addEventListener('mouseleave', function() {
+            mouseleave(this.parentNode);
+        });
+    }
+    document.addEventListener("keydown", function(event) {
+        var key = event.key.toUpperCase();
+        var code = key.charCodeAt();
+        console.log(`${key} ${code} down`);
+        if (event.repeat) {
+            return;
+        }
+        if (event.ctrlKey || event.altKey || event.metaKey) {
+            return;
+        }
+        if (key2note.has(code)) {
+            notedown(code, key2note.get(code), env.velocity);
+        }
+        if (key == '-') {
+            if (env.velocity > 0) env.velocity--; 
+            refresh();
+        }
+        if (key == '=' || key == '+') {
+            if (env.velocity < 9) env.velocity++;
+            refresh();
+        }
+    });
+    document.addEventListener("keyup", function(event) {
+        var key = event.key.toUpperCase();
+        var code = key.charCodeAt();
+        console.log(`${key} ${code} up`);
+        if (key2note.has(code)) {
+            noteup(code);
+        }
+    });
+}
+piano.load.then(() => {
+    after_load();
+});
+
+function fetch_input() {
+    let inputs = {
+        main: extract(document.getElementById("input").value),
+        sub: decompress(extract(document.getElementById("input2").value)),
+    };
+    return inputs;
+}
+document.getElementById("submit").onclick = () => {
+    env.bpm = parseInt(document.getElementById("bpm").value);
+    const selectElement = document.getElementById('offset_option');
+    const selectedOption = selectElement.options[selectElement.selectedIndex];
+    if (selectedOption.value == "flex") {
+        env.global_offset = parseInt(document.getElementById("key_offset").value);
+        env.fixed_offset.fill(0);
+    } else {
+        env.global_offset = 0;
+        env.fixed_offset.fill(0);
+        var cnt = parseInt(document.getElementById("key_offset").value);
+        //console.log(cnt);
+        if (cnt > 0) {
+            for (var i = 0; i < cnt; i++) {
+                env.fixed_offset[sharp_note[i]] = 1;
+            }
+        } else if (cnt < 0) {
+            for (var i = 0; i < (-cnt); i++) {
+                env.fixed_offset[flat_note[i]] = -1;
+            }
+        }
+        //console.log(env.fixed_offset);
+    }
+    env.time1 = parseInt(document.getElementById("time_sign1").value);
+    env.time2 = parseInt(document.getElementById("time_sign2").value);
+    refresh();
+}
+
+document.getElementById("stop").onclick = () => {
+    console.log("stop");
+    stop();
+}
+document.getElementById("tutorial").onclick = () => {
+    init();
+    document.getElementById("input").value = tutorial;
+    refresh();
+};
+document.getElementById("tutorial2").onclick = () => {
+    init();
+    document.getElementById("input2").value = tutorial2;
+    refresh();
+};
+document.getElementById("bwv846").onclick = () => {
+    env.bpm = 70;
+    env.time1 = 4;
+    env.time2 = 4;
+    document.getElementById('offset_option').selectedIndex = 0;
+    env.fixed_offset.fill(0);
+    env.global_offset = 0;
+    document.getElementById("input").value = bwv846;
+    document.getElementById("input2").value = "";
+    refresh();
+    //console.log(sampler.instrumentNames);
+    //context.resume();
+};
+document.getElementById("haruhikage").onclick = () => {
+    env.bpm = 90;
+    env.time1 = 6;
+    env.time2 = 8;
+    document.getElementById('offset_option').selectedIndex = 0;
+    env.fixed_offset.fill(0);
+    env.global_offset = -1;
+    document.getElementById("input").value = haruhikage;
+    document.getElementById("input2").value = "";
+    refresh();
+    //play(haruhikage);
+    //console.log(sampler.instrumentNames);
+    //context.resume(); // enable audio context after a user interaction
+};
+document.getElementById("reset").onclick = () => {
+    init();
+};
+document.getElementById("start").onclick = () => {
+    console.log("click");
+    //getAttribute();
+    var input = fetch_input();
+    play(input.main);
+}
+document.getElementById("gamestart").onclick = () => {
+  alert('还没写呢');
+  var input = fetch_input();
+  console.log(input);
+  localStorage.setItem('tape', JSON.stringify(input));
+  localStorage.setItem('env', JSON.stringify(env));
+  localStorage.setItem('difficulty', document.getElementById("difficulty-select").selectedIndex);
+  window.location.href = './game.html'
+}
+
